92e254aa9be32785fce2456536d107ebf0864488
Magnil <lovcrimson@gmail.com>
2013-05-15 20:55:40 +0800

    Site updated at 2013-05-15 12:55:40 UTC

--- javascripts/html2canvas.js
+++ javascripts/html2canvas.js
@@ -2630,7 +2630,7 @@ window.html2canvas = function(elements, opts) {
     background: "#fff",
 
     // preload options
-    proxy: "http://xiaoxiaoyue.cn/html2canvas/proxy.php",
+    proxy: null,
     timeout: 0,    // no timeout
     useCORS: false, // try to load images as CORS (where available), before falling back to proxy
     allowTaint: false, // whether to allow images to taint the canvas, won't need proxy if set to true
